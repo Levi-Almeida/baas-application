97bac9f852a7e5a0aa733a1e73db4a499f038a74
Levi-Almeida <levi02almeida@gmail.com>
2026-08-12 12:35:33 -0300

    fix: track backend as regular directory

---
+++ backend/src/main.ts
@@ -0,0 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('VBA BaaS API')
+    .setDescription('API Banking as a Service integrada ao gateway Lera Box')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
